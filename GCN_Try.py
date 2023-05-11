427310a9061fefbf97084acddca919a502e7e11e
RenQuanzhen <132890097+RenQuanzhen@users.noreply.github.com>
2023-05-11 11:26:17 +0800

    Update GCN_Try.py

--- GCN_Try.py
+++ GCN_Try.py
@@ -6,10 +6,9 @@ from GaborNet import GaborConv2d
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchsummary import summary
-from PIL import Image
-from torchvision import transforms
 from GaborModels import *
 
+# classes = (1:'plane',2:'car',3:'bird',4:'cat',5:'deer',6:'dog',7:'frog',8:'horse',9:'ship',10:'truck')
 train_data = torchvision.datasets.CIFAR10(root="data", train=True, transform=torchvision.transforms.ToTensor(),
                                           download=True)
 test_data = torchvision.datasets.CIFAR10(root="data", train=False, transform=torchvision.transforms.ToTensor(),
@@ -27,102 +26,56 @@ print("测试数据集的长度为：{}".format(test_data_size))
 train_dataloader = DataLoader(train_data, batch_size=64)
 test_dataloader = DataLoader(test_data, batch_size=64)
 
-# #搭建网络：在 VGG16 的基础上将前两层变化为Gabor卷积网络-》论文中
-# class GCN_test(nn.Module):
-#     def __init__(self):
-#         super(GCN_test, self).__init__()
-#         self.features=nn.Sequential(
-#             GaborConv2d(in_channels=3, out_channels=64, kernel_size=(3, 3),padding=(1,1)),
-#             nn.ReLU(inplace=True),
-#             GaborConv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
-#             nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn. Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
-#             nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
-#             nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
-#             nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-#         )
-#         self.avgpool=nn.AdaptiveAvgPool2d(output_size=(7, 7))
-#         self.classifier=nn.Sequential(
-#             nn.Flatten(),# 展开，将图片转化为一维线性
-#             nn.Linear(in_features=25088, out_features=4096, bias=True),
-#             nn.ReLU(inplace=True),
-#             nn.Dropout(p=0.5, inplace=False),
-#             nn.Linear(in_features=4096, out_features=4096, bias=True),
-#             nn. ReLU(inplace=True),
-#             nn.Dropout(p=0.5, inplace=False),
-#             nn.Linear(in_features=4096, out_features=10, bias=True), #在 CIFAR10上训练
-#         )
-#
-#     def forward(self, x):
-#         x=self.features(x)
-#         x=self.avgpool(x)
-#         x=self.classifier(x)
-#         return x
+#数据集所分类数目
+Class=10
+
+# 训练的轮数
+epoch = 10
+
 # #创建网络 1
 # gcn_test=GCN_test()
 #创建网络 2
 #gcn_test_2=Gabor_vgg()
 #创建网络 3
-gcn_test_3=GCN13()
+# gcn_test_3=GCN13()
 #创建网络 4
-device=torch.device('cuda'if torch.cuda.is_available() else "cpu")#电脑主机的选择
 VGG16=vgg16(True, progress=True,num_classes=10)#定于分类的类别
+#创建网络 5
+gcn_test_4=gcn13_2(Class)
 
-#测试数据
-# test_pth="Dataset/train/ants/0013035.jpg"
-# img_test=Image.open(test_pth)
-# tensor_trans=torchvision.transforms.ToTensor()   #利用创建好的工具将图片文件转化为tensor类型
-# img_tensor=tensor_trans(img_test)
-# print(img_tensor.shape)
-# img=torch.reshape((img_tensor),(-1,3,32,32))
-# print(img.shape)
-# gcn_test(img)
-# summary(gcn_test,(3,32,32))  #输入图片经过每层后尺寸的变化
-
-#
 # 损失函数
 loss_fn = nn.CrossEntropyLoss()
 
-# 优化器
+#学习率
+warm_up = 0.05
+decay_rate = 0.95
+decay_steps = 20
+# 自定义学习率衰减算法
+def self_adjust_learning_rate(optimizer, train_sum):
+    warm_up_step = int(epoch * warm_up)
+    if train_sum < warm_up_step:
+        lr = 0.1 * LR + (LR - 0.1 * LR) / warm_up_step * train_sum
+    else:
+        lr = LR * (decay_rate ** ((train_sum - warm_up_step) // decay_steps))
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+    return lr
 # learning_rate = 0.01
 # 1e-2=1 x (10)^(-2) = 1 /100 = 0.01
-learning_rate = 1e-2
+LR = 1e-2  #初始学习率数值
+# 优化器
 #optimizer = torch.optim.SGD(gcn_test.parameters(),learning_rate)  #网络1
 #optimizer = torch.optim.SGD(gcn_test_2.parameters(),learning_rate)  #网络2
 # optimizer = torch.optim.SGD(gcn_test_3.parameters(),learning_rate)  #网络3
-optimizer = torch.optim.SGD(VGG16.parameters(),learning_rate)  #网络3
-
+# optimizer = torch.optim.SGD(VGG16.parameters(),lr=LR)  #网络4
+optimizer = torch.optim.SGD(gcn_test_4.parameters(),lr=LR)  #网络5
 
 # 设置训练网络的一些参数
 # 记录训练的次数
 total_train_step = 0
 # 记录测试的次数
 total_test_step = 0
-# 训练的轮数
-epoch = 10
+
 
 # 添加tensorboard
 writer = SummaryWriter("gcn_train")
@@ -133,17 +86,20 @@ for i in range(epoch):  #大循环：训练轮数
     #gcn_test.train()
     #gcn_test_2.train()
     #gcn_test_3.train()
-    VGG16.train()
+    # VGG16.train()
+    gcn_test_4.train()
     for data in train_dataloader: #小循环：遍历训练集中的每一张图片
         imgs, targets = data
         optimizer.zero_grad()
         # outputs =gcn_test(imgs)
         # outputs = gcn_test_2(imgs)
         # outputs = gcn_test_3(imgs)
-        outputs = VGG16(imgs)
+        # outputs = VGG16(imgs)
+        outputs = gcn_test_4(imgs)
         loss = loss_fn(outputs, targets)
         loss.backward()
         optimizer.step()
+        lr = self_adjust_learning_rate(optimizer, i)  # 自定义学习率衰减算法
 
         total_train_step = total_train_step + 1
         if total_train_step % 100 == 0:  #每百次输出一次结果
@@ -154,7 +110,8 @@ for i in range(epoch):  #大循环：训练轮数
        #  gcn_test.eval()
         #gcn_test_2.eval()
         # gcn_test_3.eval()
-        VGG16.eval()
+        # VGG16.eval()
+        gcn_test_4.eval()
         total_test_loss = 0
         total_accuracy = 0  #测试网络模型的精确度
 
@@ -164,7 +121,8 @@ for i in range(epoch):  #大循环：训练轮数
             # outputs =gcn_test(imgs)
             #outputs = gcn_test_2(imgs)
             # outputs = gcn_test_3(imgs)
-            outputs = VGG16(imgs)
+            # outputs = VGG16(imgs)
+            outputs=gcn_test_4(imgs)
             loss = loss_fn(outputs, targets)
             total_test_loss = total_test_loss + loss.item()
             accuracy = ((outputs.argmax(1) == targets).sum()).clone().detach().cpu().numpy() # argmax(1)表示按行进行读取
@@ -182,8 +140,10 @@ for i in range(epoch):  #大循环：训练轮数
     #torch.save(gcn_test_2,"Result_GCN_test2/GCN_test{}.pth".format(i))  #保存当前训练好的网络
     #print("模型已保存")
     #torch.save(gcn_test_3, "Result_GCN_test3/GCN_test{}.pth".format(i))
-    torch.save(vgg16, "Result_VGG16_test/VGG16_test{}.pth".format(i))
+    # torch.save(vgg16, "Result_VGG16_test/VGG16_test{}.pth".format(i))
+    torch.save(gcn_test_4, "Result_GCN_test4/GCN_test{}.pth".format(i))
     print("模型已保存")
+    # torch.load("Result_VGG16_test/VGG16_test{}.pth".format(i))  # 加载已保存的模型
 
 writer.close()
 
